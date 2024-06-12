b14cf30765927afc44eaeffc0530f56d4cd1782c
Thiago-Gomes-Araujo <thiagoogmes543@hotmail.com>
2024-06-12 13:22:24 -0400

    BO na Aula 27 - CRUD ADDREESES

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,10 @@
 class Contact < ApplicationRecord
     belongs_to :kind
     has_many :phones
-    accepts_nested_attributes_for :phones
+    has_one :address
+    accepts_nested_attributes_for :phones, allow_destroy: true
+    accepts_nested_attributes_for :address, allow_destroy: true
+
     
     def hello
         I18n.t('hello')
