43ff2ef2d252097ace32fece39da80c89d8baf5f
Thiago-Gomes-Araujo <thiagoogmes543@hotmail.com>
2024-06-13 08:34:11 -0400

    I18n aprenstando erro de tradução

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,13 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :birthdate
+  attributes :id, :name, :email, :birthdate
 
-  # def attributes(*args)
-  #   h = super(*args)
-  #   h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
-  #   h
-  # end
+  def attributes(*args)
+    h = super(*args)
+    h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+    h
+  end
 end
+
+
+# h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+# h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
