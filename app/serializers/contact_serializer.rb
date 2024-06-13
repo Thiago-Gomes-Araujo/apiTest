4a539a1c5de332eb728af0a0f87c6606e41718b6
Thiago-Gomes-Araujo <thiagoogmes543@hotmail.com>
2024-06-13 09:16:34 -0400

    Adc meta ( Informação extras sobre o model)

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,11 +1,17 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthdate
-  
+  attributes :id, :name, :email, :birthdate #, :author
+
 # Associações
   belongs_to :kind
   has_many :phones
   has_one :address
 
+  # def author
+  #   "Thiago"
+  # end
+  meta do
+    { author: "Thiago Gomes"}
+  end
   def attributes(*args)
     h = super(*args)
     h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
