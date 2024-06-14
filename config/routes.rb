9115c2a3062f536a4a385090f45975fc533c5e66
Thiago-Gomes-Araujo <thiagoogmes543@hotmail.com>
2024-06-14 08:11:23 -0400

    criando rotas /contacts/id/kind

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :kinds
-  resources :contacts
+
+  # Adc rota
+  resources :contacts do
+    resource :kind, only: [:show]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
